534942cb592098aa6eabd50b11e02f7392ba7882
Will Burke <billyburke1012@gmail.com>
2021-01-19 13:10:23 -0500

    lunch updates

---
+++ README.md
@@ -0,0 +1,14 @@
+## Callback
+
+- is an argument that is a function
+
+Does not have to be an arrow function
+Will sometimes be an anonymous function
+
+## Promises
+
+- it's an object
+- with a .then() fn
+    - pass it a callback fn
+- and it has a .catch() fn
+    - 
--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,33 @@
 console.log('trying a new message');
+
+function processResponse(response) {
+    return response.json();
+}
+
+//show the joke on the page
+// 1. make the request using fetch()
+
+fetch('https://icanhazdadjoke.com', {
+    headers: {
+        Accept: 'application/json'
+    }
+})
+.then(response => {
+    const theData = response.json();
+    return theData; // returns to the next .then()
+}) //convert to JSON
+.then(data => {
+    console.log(data);
+    return data.joke;
+})
+.then(theJoke => {
+    console.log(theJoke);
+})
+.catch(err => console.log(err))
+
+// 2. grab the .joke 
+// 3. render it to the page
+
+
+
+
